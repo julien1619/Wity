6c1806b9792d65fd98f98b2f5c96e4c9da700644
Julien Blatecky <julien1619@gmail.com>
2013-01-21 05:47:51 -0500

    postit position and editing fixed
    Signed-off-by: Julien Blatecky <julien1619@gmail.com>

--- src/logic.js
+++ src/logic.js
@@ -1,56 +1,73 @@
 $(document).ready( function() {
 
-	var socket = io.connect(document.URL);
+    /**
+     * Global vars initialization
+     */
+    var socket = io.connect(document.URL);
     var locked = {};
 	
-	socket.on('boxAdded', function (data) {
-		console.log(data);
-		addBox(data.id,data.x,data.y,data.content);
-        locked[data.id] = false;
-	});
-	
-	socket.on('boxRemoved', function (data) {
-		console.log(data);
-		removeBox(data.id);
-	});
-	
-	socket.on('boxChanged', function (data) {
-		console.log(data);
-		changePostIt(data.id,data.x,data.y,data.content);
-	});
-	
-	function sendAddBox(xPos,yPos,content) {
+    /**
+     * Add an object in view
+     */
+    
+    //Requesting add operation
+    function sendAddBox(xPos,yPos,content) {
         var data = {"x":xPos,"y":yPos,"content":content};
         socket.emit('add box',data);
-	}
+    }
+    
+    //Applying add operation
+	socket.on('boxAdded', function (data) {
+		$("#postit_container").append("<div id='wity_"+data.id+"' class='postit'><div class='postit_header'></div><div class='postit_content'>"+data.content+"</div></div>");
+        $("#wity_"+data.id).css('left',data.x);
+        $("#wity_"+data.id).css('top',data.y);
+        locked[data.id] = false;
+	});
+    
+    /**
+     * Remove an object in view
+     */
 	
-	function sendRemoveBox() {
-		socket.emit('remove box');
+    //Requesting remove operation
+    function sendRemoveBox() {
+        socket.emit('remove box');
 	}
+    
+    //Applying remove operation
+	socket.on('boxRemoved', function (data) {
+		$("#wity_"+data.id).remove();
+	});
+    
+    /**
+     * Update an object in view
+     */
 	
-	function sendChangePostIt(id, xPos, yPos, content) {
-		console.log("Request changing post it");
+    //Requesting update operation
+    function sendChangePostIt(id, xPos, yPos, content) {
         var data = {"id": id,"x":xPos,"y":yPos,"content":content};
 		socket.emit('change box', data);
 	}
+    
+    //Applying update operation
+	socket.on('boxChanged', function (data) {
+		$("#wity_"+data.id).css('left',data.x);
+        $("#wity_"+data.id).css('top',data.y);
+        $("#wity_"+data.id+" > .postit_content").html(data.content);
+	});
 	
-	//Change color button
-	$(".postit").live("click", function (event) {
-        var idSubstr = $(this).attr('id').substring(5);
-        console.log("idSubstr:"+idSubstr);
-		var id = parseInt(idSubstr);
+	//Change text
+	$(".postit").on("click", function (event) {
+        var id = getId(event.target);
+        console.log(id + " will be edited.");
         
         if(!locked[id]) {
             //sendLock(id);
-            editContent(id);
+            $("#postit_id").val(id);
+            $("#postit_editor").val($("#wity_"+id+" > .postit_content").html());
         }
 	});
     
-    function editContent(id) {
-        $("#postit_id").val(id);
-        $("#postit_editor").val($("#wity_"+id+" > .postit_content").html());
-    }
-    
+    //Save text
     $("#save_button").click(function () {
         if($("#postit_id").val()!=="" || $("#postit_id").val()!==undefined) {
             var id = $("#postit_id").val();
@@ -61,30 +78,24 @@ $(document).ready( function() {
             sendChangePostIt(id, xPos, yPos, content);
         }
     });
-	
-	//Add logic
-	function addBox(id, xPos, yPos, content) {
-        console.log("Add box! x:"+xPos+" y:"+yPos+" id:"+id);
-		$("#postit_container").append("<div id='wity_"+id+"' class='postit'><div class='postit_header'></div><div class='postit_content'>"+content+"</div></div>");
-		$("#wity_"+id).css('left',xPos);
-        $("#wity_"+id).css('top',yPos);
-	}
-	
-	//Remove logic
-	function removeBox(id) {
-		$("#wity_"+id).remove();
-	}
-	
-	//Change color button
-	function changePostIt(id, xPos, yPos, content) {
-		$("#wity_"+id).css('left',xPos);
-        $("#wity_"+id).css('top',yPos);
-        $("#wity_"+id+" > .postit_content").html(content);
-	}
     
-    $("#background_wrapper").click(function(event) {        
-        var x = event.pageX - $(this).offset().left;
-        var y = event.pageY - $(this).offset().top; 
+    //Add postit onClick
+    $("#background_wrapper").click(function(event) {   
+        var xOffset = parseInt(nCss("width","postit"))/2;
+        var yOffset = parseInt(nCss("height","postit"))/2;
+        
+        var maxXPos = parseInt($(this).css("width")) - xOffset*2;
+        var maxYPos = parseInt($(this).css("height")) - yOffset*2;
+        
+        var x = event.pageX - $(this).offset().left - xOffset;
+        var y = event.pageY - $(this).offset().top - yOffset; 
+        
+        console.log(xOffset + " " + yOffset + " " + maxXPos + " " + maxYPos + " " + x + " " + y + " ");
+        
+        x = x>maxXPos ? maxXPos : x;
+        x = x<0 ? 0 : x;
+        y = y>maxYPos ? maxYPos : y;
+        y = y<0 ? 0 : y;
         
         sendAddBox(x,y,"");
     });
@@ -106,4 +117,27 @@ $(document).ready( function() {
             sendChangePostIt(id, xPos, yPos, content);
         }
     });*/
+    
+    /**
+     * Utils
+     */
+     
+    //Get wity id
+    function getId(object) {
+        console.log(object);
+        var idSubstr = object.attr('id').substring(5);
+        console.log("idSubstr:"+idSubstr);
+        return parseInt(idSubstr);
+    }
+    
+    function nCss(prop, fromClass) {
+
+        var $inspector = $("<div>").css('display', 'none').addClass(fromClass);
+        $("body").append($inspector); // add to DOM, in order to read the CSS property
+        try {
+            return $inspector.css(prop);
+        } finally {
+            $inspector.remove(); // and remove from DOM
+        }
+    }
 });
